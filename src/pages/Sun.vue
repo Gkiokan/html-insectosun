0a4e96aca4290f51cb98a33e4d4e80f2aa9ede4f
Gkiokan Sali <gkiokan@hotmail.de>
2022-07-11 12:46:03 +0200

    sun products done

--- src/pages/Sun.vue
+++ src/pages/Sun.vue
@@ -1,10 +1,113 @@
 <template lang="html">
 <q-page>
 
-    <TopHeader title="Sonnenschutz" image="/img/plisses/Plissees_Header_R-0776_10.1045.jpg.24289799.jpg" />
+    <TopHeader title="Sonnenschutz" image="/img/plisses/Plissees-im-Wohnzimmer-weiss-schwarz-erfal.jpg.24293455.jpg" image-old="/img/plisses/Plissees_Header_R-0776_10.1045.jpg.24289799.jpg" />
 
     <div class='q-pa-md'>
       <Container>
+
+          <q-space style="height: 50px" />
+          <ProduktInfo title="Plisse - Grandioses Farbenspiel" :gallery="plisseGallery">
+            Plissees gehören zu den beliebtesten Modellen unter den Sonnenschutzprodukten,
+            da sie sehr vielseitig einsetzbar sind. Eine Plisseeanlage besteht aus einer
+            speziellen Sonnenschutztextilie mit eingearbeiteten, dauerhaften Falten. <br>
+            <br>
+            Diese kann wie eine Ziehharmonika auf ein schmales Paket zusammengeschoben werden
+            oder auseinandergezogen das Fenster teilweise oder vollständig bedecken. Der Stoff
+            ist dabei oben und unten an schmalen Schienen befestigt und wird mittels einer
+            speziellen Schnurtechnik geführt. Die maßgefertigten Anlagen lassen sich stufenlos
+            am Fenster positionieren und leicht bedienen.<br>
+            <br>
+            Erleben Sie wohnliche Vielfalt mit Plissees von erfal und entscheiden Sie ganz individuell nach Stimmung, Situation oder Jahreszeit, wie viel Licht einfallen soll.
+            <br>
+            <ul>
+              <li>Stabil und langlebig</li>
+              <li>stufenlos positionierbar</li>
+              <li>maßgenaue Fertigung für jede Fensterform</li>
+              <li>platzsparende Befestigung</li>
+            </ul>
+          </ProduktInfo>
+          <q-space style="height: 100px" />
+
+
+          <ProduktInfo title="Flächenvorhang - Spielerisch verschieben" :gallery="vorhangGallery">
+            Flächenvorhänge sind die moderne Alternative zum klassischen Vorhang und haben ihren
+            Ursprung in Asien. Glatte Stoffbahnen, so genannte Paneele, werden mit Hilfe von Schiebesystemen,
+            auch als Paneelwagen bezeichnet, in einer Schiene geführt. Sie lassen sich flexibel seitlich
+            verschieben, hintereinander positionieren und können bei Bedarf die ganze oder nur Teile der
+            Fensterfläche bedecken.<br>
+            <br>
+            Große Fensterfronten und Glasflächen sind wie geschaffen für großzügige Flächenvorhänge. <br>
+            <br>
+            Sie eignen sich perfekt für moderne Wohnungen und Büros und sind eine reizvolle Alternative zur klassischen Gardine.
+            Alternativ lassen sie sich auch als Raumteiler einsetzen und bringen so neue Flexibilität in alle Räume. <br>
+            <br>
+            Viele Farben mit unterschiedlichen Oberflächen laden ein zum Kombinieren.
+          </ProduktInfo>
+          <q-space style="height: 100px" />
+
+
+          <ProduktInfo title="Vertikaljalousie - Sachliche Linienführung" :gallery="lamellenGallery">
+            Ein Lamellenvorhang besteht aus schmalen senkrecht verlaufenden Stoffbahnen, welche an
+            einer Führungsschiene befestigt sind. Die Lamellen können durch einfaches Verschieben
+            in der Schiene einen Teil oder das ganze Fenster beschatten. <br>
+            <br>
+            Die einzelnen Lamellen lassen sich durch vertikales Wenden stufenlos einstellen. Daher wird der Lamellenvorhang
+            häufig auch als Vertikaljalousie bezeichnet. Je nach Stellung der Lamellen können Sie individuell den Lichteinfall
+            regulieren und der Blick nach draußen ist trotzdem möglich. <br>
+            <br>
+            Typisch für Büros, Konferenz- oder Behandlungszimmer haben Lamellenvorhänge längst auch im privaten Bereich Einzug gehalten.
+            Durch die sachliche Linienführung gelten sie als besonders modern und zeitgemäß.
+            Erzielen Sie interessante Lichteffekte durch stufenloses Verstellen der Lamellen. <br>
+            <br>
+            Zahlreiche Farbvarianten und unterschiedliche Muster für jeden Einrichtungsstil.<br>
+          </ProduktInfo>
+          <q-space style="height: 100px" />
+
+
+          <ProduktInfo title="Jalousie - Klassisch modern" :gallery="jalousieGallery">
+            Jalousien ermöglichen eine optimale und einfache Feinregulierung von Licht und Sicht.
+            Damit wählen Sie einen zeitlosen und zugleich dekorativen Sonnenschutz mit modernem Charakter.
+            Viele verschiedene Farben und Lamellenbreiten sowie Farbwechsel bieten persönliche Gestaltungsfreiheit. <br>
+            <br>
+            Aluminium-Jalousien sind beliebte Klassiker im Sicht- und Sonnenschutz. Horizontal angeordneten
+            Lamellen, durch Schnüre oder Leiterbänder verbunden, sind beweglich und lassen sich beliebig
+            schwenken. Eine Jalousie lässt sich ebenso komplett zusammenschieben und gibt das Fenster frei. <br>
+            <br>
+            Jalousien aus Aluminium sind langlebig und in verschiedenen Farben und Breiten erhältlich.
+          </ProduktInfo>
+          <q-space style="height: 100px" />
+
+          <ProduktInfo title="Rollo - Dekoratives Rollenspiel" :gallery="rolloGallery">
+            Ein Klassiker mit Tradition. Technische Weiterentwicklungen haben dieses Sonnenschutzelement perfektioniert.<br>
+            <br>
+            Eine Rolloanlage besteht aus einem rechteckigen Stoff, der auf eine Welle gewickelt ist.
+            Mit Hilfe verschiedener Bedienvarianten lässt sich der Rollostoff leicht von der Welle abrollen und
+            auf die gewünschte Höhe positionieren. Das spezielle Doppel - Rollo ermöglicht durch gegeneinander
+            laufenden Stoff ein faszinierendes Spiel mit Licht und Schatten. <br>
+            <br>
+            Erleben Sie die Vielfalt an Modellen und Möglichkeiten des Rollos und profitieren Sie von
+            flächiger Lichtregulierung und hohem Bedienkomfort. Durch eine große Auswahl an Farben, Mustern,
+            Geweben und Volants setzen Sie in Ihren Räumen wirkungsvolle Akzente.
+
+          </ProduktInfo>
+          <q-space style="height: 100px" />
+
+
+          <ProduktInfo title="Verdunklungsanlagen" :gallery="dunkelGallery">
+            Industriegebäude, aber auch öffentliche Einrichtungen wie beispielsweise Veranstaltungshallen
+            oder Schulen, verfügen einerseits häufig über große Glasfassaden, andererseits werden Forderungen
+            hinsichtlich einer vollständigen Verdunklung der Räume gestellt.<br>
+            <br>
+            Die für diesen Anwendungsbereich entwickelten Verdunklungsanlagen sind in unterschiedlichen
+            Modellen erhältlich und können sowohl vertikal als auch horizontal montiert werden.<br>
+            <br>
+            Senkrecht- oder Plafondanlagen lassen sich mit Gurtbedienung, Kurbelantrieb oder Motorsteuerung
+            (auch als Gruppe oder Funk bedienbar) leicht öffnen oder schließen.<br>
+            <br>
+            Unsere Kollektion wurde speziell für den Objektbereich und dessen Anforderungen zusammengestellt und umfasst diesbezüglich optimal geeignete Textilgewebe.
+          </ProduktInfo>
+          <q-space style="height: 100px" />
 
       </Container>
     </div>
@@ -13,6 +116,100 @@
 
 <script>
 export default {
+    data(){ return {
+        plisseGallery: [
+          '/img/plisses/plissee-kinderzimmer-kindersicherheit.jpg.24281362.jpg',
+          '/img/plisses/Plissee-mit-griffleiste-rot-weiss.20872544.jpg.24281363.jpg',
+          '/img/plisses/Plissee-mit-Seitenfuerung-Dachfenster.jpg.24281361.jpg',
+          '/img/plisses/plissee-oberlicht-fenster-wintergarten.jpg.24281355.jpg',
+          '/img/plisses/plissee-runde-fenster.jpg.24281353.jpg',
+          '/img/plisses/plissee-weiss-rot-kueche-erfal.jpg.24281352.jpg',
+          '/img/plisses/Plissees_Bauarten_D-0475_20.655.jpg.24279697.jpg',
+          '/img/plisses/Plissees_Bauaten_D-0111_20.jpg.24279695.jpg',
+          '/img/plisses/plissees-dachfenster-dreieckfenster-blauer-stoff.jpg.24281356.jpg',
+          '/img/plisses/Plissees-runde-Fenster-anfertigung-erfal.jpg.24281708.jpg',
+          '/img/plisses/Sonenschutz-mit-Plissees-von-erfal.jpg.24283365.jpg',
+          '/img/plisses/sonnenschutz-plissee-erfal.jpg.24281357.jpg',
+        ],
+
+        vorhangGallery: [
+          '/img/vorhang/erfal_Flächenvorhänge_Bauarten_Besonderheiten_Flexilibität.jpg.24279878.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Bauarten_Besonderheiten_Individualdruck.jpg.24279881.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Bauarten_Besonderheiten_Paneelwagen.jpg.24279877.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Ganz_ihr_Geschmack_Farben_Muster.jpg.24282261.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Ganz_ihr_Geschmack_Farbwechsel.jpg.24282260.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Ganz_ihr_Geschmack_Ferbverlauf.jpg.24281898.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Ganz_ihr_Geschmack_Transparenzen.jpg.24281897.jpg',
+          // '/img/vorhang/erfal_Flächenvorhänge_Header.jpg.24291953.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Montagemöglichkeiten_Deckenmontage.jpg.24279872.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Montagemöglichkeiten_Wandmontage.jpg.24279871.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Produktcharakteristik.jpg.24281883.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge_Produktvorteile.jpg.24281884.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge__Bauarten_Besonderheiten_Biegungen.jpg.24280243.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge__Bauarten_Besonderheiten_Flächenvorhänge_und_mehr.jpg.24280245.jpg',
+          '/img/vorhang/erfal_Flächenvorhänge__Bauarten_Besonderheiten_Raumteiler.jpg.24279879.jpg',
+          // '/img/vorhang/erfal_Flächenvorhänge__Bauarten_Besonderheiten_Segmente.jpg.24280244.jpg',
+          // '/img/vorhang/erfal_Flächenvorhänge__Bauarten_Besonderheiten_Zebra_Technik.jpg.24279880.jpg',
+        ],
+
+        lamellenGallery: [
+          '/img/lamellen/erfal Lamellenvorhänge 4 eckig.24306408.jpg.24306416.jpg',
+          '/img/lamellen/erfal Lamellenvorhänge Cube.jpg.24306446.jpg',
+          '/img/lamellen/erfal Lamellenvorhänge Wandbefestigung.jpg.24305956.jpg',
+          '/img/lamellen/erfal Lamellenvorhänge im Bogenfenster Header.jpg.24305946.jpg',
+          '/img/lamellen/erfal Lamellenvorhänge mit Farbwechsel.jpg.24305970.jpg',
+          '/img/lamellen/erfal Lamellenvorhänge5.jpg.24306001.jpg',
+          '/img/lamellen/erfal Lamellenvorhänge6.jpg.24306386.jpg',
+          '/img/lamellen/erfal_Lamellenvorhaenge_Bauarten_Individualdruck.jpg.24279841.jpg',
+          '/img/lamellen/erfal_Lamellenvorhaenge_Bauarten_Lamelle_250mm.jpg.24279843.jpg',
+          '/img/lamellen/erfal_Lamellenvorhaenge_Bauarten_Sonderformen.jpg.24279842.jpg',
+          '/img/lamellen/erfal_Lamellenvorhaenge_Montage_Deckenbefestigung.jpg.24281850.jpg',
+          '/img/lamellen/erfal_Lamellenvorhaenge_Montage_Deckeneinbau.jpg.24281851.jpg',
+          '/img/lamellen/erfal_Lamellenvorhaenge_Produktcharakteristik.jpg.24281845.jpg',
+          '/img/lamellen/erfal_Lamellenvorhänge_Ganz_ihr_Geschmack_Material_Ausrüstung_R-0007.jpg.24282263.jpg',
+        ],
+
+        jalousieGallery: [
+          '/img/jalousie/Aluminium-Jalousien-gelb-Bauarten_Besonderheiten_Dachfenster.jpg.24280199.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Bauarten_Besonderheiten_Flexx_Line.jpg.24279962.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Bauarten_Besonderheiten_Slope.jpg.24279961.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Ganz_ihr_Geschmack_Farbwechsel.jpg.24279960.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Ganz_ihr_Geschmack_Leiterband.jpg.24279959.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Ganz_ihr_Geschmack_Material_Muster.jpg.24280291.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Montage_Glasleiste.jpg.24281992.jpg',
+          '/img/jalousie/Bildauswahl_Aluminium-Jalousien_Montage_vor_das_Fenster.jpg.24281971.jpg',
+          '/img/jalousie/blaue-Jalousien-Fenster-Bauarten_Besonderheiten_Mega_view.jpg.24281567.jpg',
+          '/img/jalousie/erfal_Aluminium-Jalousien_Montage_Fensterrahmen.jpg.24281991.jpg',
+          '/img/jalousie/erfal_Aluminium-Jalousien_bauarten_besonderheiten_Kindersicherheit.jpg.24279964.jpg',
+        ],
+
+        rolloGallery: [
+          '/img/rollos/erfal_Rollos_12.jpg.24279857.jpg',
+          '/img/rollos/erfal_Rollos_Bauarten_Besonderheiten_Individualdruck.jpg.24279860.jpg',
+          '/img/rollos/erfal_Rollos_Header.jpg.24291934.jpg',
+          '/img/rollos/erfal_Rollos_Montage_azf_Glasleiste.jpg.24282063.jpg',
+          '/img/rollos/erfal_Rollos_Montagemöglichkeit_Fensterrahmen.jpg.24281867.jpg',
+          '/img/rollos/erfal_Rollos_Montagemöglichkeit_vor_Fenster.jpg.24281866.jpg',
+          '/img/rollos/erfal_Rollos_Produktcharakteristik.jpg.24281864.jpg',
+          '/img/rollos/erfal_Rollos_Produktvorteile.jpg.24281865.jpg',
+          '/img/rollos/erfal_Rollos_bauarteb_besonderheiten_Dachfenster.jpg.24279855.jpg',
+          '/img/rollos/erfal_Rollos_bauarten_besonderheiten_Kindersicherheit.jpg.24279858.jpg',
+          '/img/rollos/erfal_Rollos_bauarten_besonderheiten_Massanfertigung.jpg.24279854.jpg',
+          '/img/rollos/erfal_Rollos_bauarten_besonderheiten_Rollos_mehr.jpg.24279740.jpg',
+          '/img/rollos/erfal_Rollos_bauarten_besonderheiten_Seitenführung.jpg.24279859.jpg',
+          '/img/rollos/erfal_Rollos_bauarten_besonderheiten_Softraise.jpg.24281148.jpg',
+          '/img/rollos/erfal_Rollos_bauarten_besonderheiten_flexilibität.jpg.24279856.jpg',
+          '/img/rollos/erfal_Rollos_ganz_ihr_geschmack_Farben_Muster.jpg.24282146.jpg',
+          '/img/rollos/erfal_Rollos_ganz_ihr_geschmack_Materialien.jpg.24281877.jpg',
+          '/img/rollos/erfal_Rollos_ganz_ihr_geschmack_Transparenzen.jpg.24281876.jpg',
+        ],
+
+        dunkelGallery: [
+          '/img/darking/erfal_Architektenportal_Objektberatung_Leistungen_Kollektion_Madrid_140-64.jpg.24282827.jpg',
+          '/img/darking/erfal_Verdunklungsanlagen_Header.jpg.24290042.jpg',
+          '/img/darking/erfal_Verdunklungsanlagen_Produktcharkteristik.jpg.24281985.jpg',
+        ],
+    }}
 }
 </script>
 
